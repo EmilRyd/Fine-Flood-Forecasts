86ad21a1a07e0f4794bb9d04e00d543496d97ad9
Emil Ryd <circlevaccess@gmail.com>
2024-09-16 14:46:05 +0200

    retrieving embeddings and toy clustering

--- experiment/embedded_finetuning/cluster.py
+++ experiment/embedded_finetuning/cluster.py
@@ -1,18 +1,18 @@
 """
 Code to cluster basins based on their embeddings
 """
-import sys
-from pathlib import Path
-sys.path.append(str(Path(__file__).parent.parent.parent))
+import numpy as np
+import matplotlib.pyplot as plt
 import torch
 from torch.utils.data import DataLoader
+from sklearn.cluster import KMeans
 from pathlib import Path
 from neuralhydrology.datasetzoo import get_dataset, camelsus
 from neuralhydrology.datautils.utils import load_scaler
-from neuralhydrology.modelzoo.cudalstm import CudaLSTM
-from neuralhydrology.modelzoo.customlstm import CustomLSTM
+
 from neuralhydrology.nh_run import start_run
-from neuralhydrology.utils.config import Config
+from experiment.exp_utils import load_cuda_model, turn_cuda_into_custom
+
 
 """
 load_model(config_file, run_dir)
@@ -22,36 +22,67 @@ generate_clusters(embeddings)
 return clusters
 """
 
+def get_embeddings(custom_stm, cfg, run_dir):
+    # get the statics embedding layer
+    custom_lstm.eval()
+    s_embedding = custom_lstm.embedding_net.statics_embedding
+    
+    # load static attributes data
+    scaler = load_scaler(run_dir)
+    dataset = get_dataset(cfg, is_train=False, period='test', scaler=scaler)
+    dataloader = DataLoader(dataset, batch_size=1, shuffle=False, collate_fn=dataset.collate_fn)
+
+    # calculate embeddings, store them in dict with the basin id as the key
+    embeddings = {}
+    with torch.no_grad():
+        for basin, features in dataset._attributes.items():
+            embedded_features = s_embedding(features)
+            embeddings[basin] = embedded_features
+    
+    return embeddings
+
+
+
+
+
+
+    # get the basins
+    print('helo')
+
 
 config_file = Path('ef_config.yml')
 
 # load trained cudalstm model
-run_dir = Path('runs') / 'embedding_experiment_1109_225229'
+# TODO figure out how where to pass these arguments
+run_dir = Path(__file__).parent / 'runs' / 'embedding_experiment_1609_112205'
 
-def load_model(config_file, run_dir):
-    # instantiate new cudalstm
-    cudalstm_config = Config(config_file)
-    cuda_lstm = CudaLSTM(cfg=cudalstm_config)
+# get trained cuda model
+(cuda_model, cfg) = load_cuda_model(config_file=config_file, run_dir=run_dir, epoch=1)
 
-    # load the trained weights into the new model
-    model_path = run_dir / 'model_epoch016.pt'
-    model_weights = torch.load(str(model_path), map_location='cuda:0') # load the weights
-    cuda_lstm.load_state_dict(model_weights) # set the new mdoel's weights
+# turn it into a custom model
+custom_lstm = turn_cuda_into_custom(cuda_model, cfg)
 
-    return cudalstm_config, cuda_lstm
+# get the embeddings from the model
+embeddings = get_embeddings(custom_lstm, cfg, run_dir)
 
-def get_embeddings(custom_stm, dataloader):
+# cluster the embeddings using K-means algorithm
 
-    embeddings = []
-    with torch.no_grad():
-        for sample in dataloader:
-            # TODO compute the embedding from the static variables, 
-            # then associate that with the corresponding basin somehow?
-            custom_lstm.embedding_net.statics_embedding(sample['x_s'])
-    print(lstm_output[0].keys())
+# see how many clusters are good
+errors = []
+max_clusters = 20
+n_clusters = range(1,max_clusters):
+for i in n_clusters:
+    kmeans = KMeans(n_clusters=i, random_state=0)
+    emb_tensors = torch.stack(list(embeddings.values()))
+    kmeans.fit(emb_tensors)
+
+    errors.append(kmeans.inertia_)
+
+plt.plot(n_clusters, errors)
+plt.show()
 
 
-    raise NotImplementedError
+
 
 def cluster(embeddings):
     
@@ -59,21 +90,8 @@ def cluster(embeddings):
 
 
 
-cudalstm_config, cuda_lstm = load_model(config_file=config_file, run_dir=run_dir)
 
-# load cudalstm weights onto custom lstm
-custom_lstm = CustomLSTM(cfg=cudalstm_config)
-custom_lstm.copy_weights(cuda_lstm)
-custom_lstm.eval()
-print(custom_lstm)
 
-# load the dataset
-scaler = load_scaler(run_dir)
-dataset = get_dataset(cudalstm_config, is_train=False, period='test', scaler=scaler)
-dataloader = DataLoader(dataset, batch_size=1000, shuffle=False, collate_fn=dataset.collate_fn)
-
-# get embeddings
-embeddings = get_embeddings(custom_lstm, dataloader)
 
 
 # sanity check that weights are the same
