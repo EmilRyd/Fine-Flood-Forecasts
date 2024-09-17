cbefb3df4056be37cc094354ed280b2a4e36899d
Gabriel <ashungafac@gmail.com>
2024-09-17 12:10:13 +0200

    more general eval function for arbitrary number of models

---
+++ experiment/eval.py
@@ -0,0 +1,96 @@
+# evaluate and compare any list of models that you wish to evaluate
+
+from pathlib import Path
+import pandas as pd
+import pickle as pkl
+import glob
+import os
+from neuralhydrology.nh_run import eval_run
+import matplotlib.pyplot as plt
+import numpy as np
+
+# Display the DataFrame with HTML rendering
+from IPython.core.display import display, HTML
+
+
+metric_goals = {
+    'NSE (median)': 1,
+    'NSE (mean)': 1,
+    'KGE (median)': 1,
+    'Pearson-r (median)': 1,
+    'Alpha-NSE (median)': 1,
+    'Beta-NSE (median)': 0,
+    'FHV (median)': 0,
+    'FLV (median)': 0,
+    'FMS (median)': 0,
+    'Peak-Timing (median)': 0
+}
+
+def bold_better(row):
+
+    # for a given metric (one row) returns a row with boldened text 
+    # for the cell which has the best score
+    vals = row.drop('Metric')
+
+    goal = metric_goals[row['Metric']]
+    
+    dists = [abs(val-goal) for val in vals]
+    best_idx = np.argmin(dists)
+    vals = [round(val, 2) for val in vals]
+    
+    new_row = [f"<b>{val}</b>" if idx==best_idx else val for idx, val in enumerate(vals)]
+    return new_row
+
+
+def evalute_models(model_eval_files: dict, include_benchmark=True, bolden_values=True):
+    """takes in a dict of model_name: eval_csvs pairs and generates a table comparing their metrics"""
+
+    comparison_df = pd.DataFrame()
+    for model, eval_file in model_eval_files.items():
+
+        assert os.path.exists(eval_file), "Metric eval file does not exist. Did you run the eval script yet?"
+        
+        # read csv
+        df = pd.read_csv(eval_file).drop(columns='basin')
+
+        # average across the columns
+        mean_ = 'mean'
+        median_ = 'median'
+        ave_df = df.copy()
+        ave_df.columns = [i + ' (' + mean_ + ')' for i in df.columns]
+        ave_df = ave_df.agg('mean', axis=0)
+        median_df = df.copy()
+        median_df.columns = [i + ' (' + median_ + ')' for i in df.columns]
+        median_df = median_df.agg('median', axis=0)
+        eval_df = pd.concat((ave_df, median_df)).reset_index(drop=False)
+        eval_df.columns = ['Metric', model] # set column labels
+
+        if comparison_df.empty:
+            comparison_df = eval_df
+        else:
+            comparison_df = pd.merge(left=comparison_df, right=eval_df, on='Metric')
+
+    if include_benchmark:
+        # read in benchmark
+        bm_df = pd.read_csv('benchmark.csv', dtype={'Benchmark': float})
+        comparison_df = pd.merge(left=comparison_df, right=bm_df, on='Metric')
+
+    if bolden_values:
+        # bolden best prediction
+        model_keys = list(model_eval_files.keys())
+        if include_benchmark:
+            model_keys.append('Benchmark')
+        comparison_df[model_keys] = comparison_df.apply(bold_better, axis=1, result_type='expand')
+
+    return comparison_df
+
+
+
+def main():
+    models = {'emb_model': Path(__file__).parent / 'embedding_model' / 'runs' / 'embedding_experiment' / 'test' / 'model_epoch030' / 'test_metrics.csv',
+              'sota_model': Path(__file__).parent / 'sota_model' / 'runs' / 'sota_20' / 'test' / 'model_epoch030' / 'test_metrics.csv'}
+    df = evalute_models(models)
+    display(HTML(df.to_html(escape=False)))
+
+
+main()
