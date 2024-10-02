20d530a9a7e1dab933c76f035d8791fda78d5f4e
Emil <emil.ryd@gmail..com>
2024-10-02 12:05:42 +0200

    attribute tuning pipeline written, started training base model now

--- experiment/finetuning/finetune.py
+++ experiment/finetuning/finetune.py
@@ -13,10 +13,11 @@ from experiment.finetuning.utils import Sweep, cfg_from_args, generate_sweep_run
 import logging
 from hyperopt import fmin, Trials, tpe, STATUS_OK
 from datetime import datetime
+from neuralhydrology.utils.config import Config
 
 
 # pick a basin
-def pick_a_basin(model: TrainedModel, lower: float = None, higher: float = None):
+def pick_a_basin(model: TrainedModel, lower: float = None, higher: float = None) -> str:
     df = pd.read_csv(model.get_eval_metrics_file(period='validation'), dtype={'basin':str})
     c_time = int(datetime.now().timestamp())
     if lower is None or higher is None:
@@ -25,9 +26,7 @@ def pick_a_basin(model: TrainedModel, lower: float = None, higher: float = None)
         basin_data = df.loc[(df['NSE'] <= higher) & (df['NSE'] >= lower)].sample(n=1, random_state=c_time)
 
     basin = basin_data.basin.iloc[0]
-    nse = basin_data.NSE.iloc[0]
-    update_files(model=model, basin=basin)
-    return basin, nse
+    return basin
 
 def update_files(model: TrainedModel, basin: str, yml_file_path=os.path.join('assets', 'finetune.yml')):
     
@@ -60,36 +59,38 @@ def finetune_model_from_cfg(data: dict, basin: str):
     with tempfile.NamedTemporaryFile(delete=True, dir=assets_dir, suffix='.yml', mode='w') as f:
         yaml.dump(data, f)  
 
-        finetune(assets_dir / f.name)
-
-        run_dir = Path(os.path.abspath('')) / 'runs' / f'basin_{basin}'
-        config_file_path = run_dir / 'config.yml'
-
-        finetuned_model = TrainedModel(config_file_path_or_experiment_name=config_file_path)
+        finetuned_model = finetune_model(assets_dir / f.name)
 
         v_df = evaluate_models([finetuned_model], basins=[basin], include_benchmark=False, period='validation', ignore_previous_metrics=True)
     
     # return negative validation score
     return {'loss': -float(v_df.iloc[0][f'basin_{basin}']), 'status': STATUS_OK, 'model': finetuned_model}
 
-def finetune_model(args):
+def finetune_model_from_args(args):
     basin = args['basin']
     data = cfg_from_args(args)
     score = finetune_model_from_cfg(data=data, basin=basin)
     return score
     
+def finetune_model(base_config_file: Path) -> TrainedModel:
+    cfg = Config(base_config_file)
+    finetune(base_config_file)
+    run_dir = Path(os.getcwd()) / 'runs' / f'{cfg.experiment_name}'
+    config_file_path = run_dir / 'config.yml'
+    finetuned_model = TrainedModel(config_file_path_or_experiment_name=config_file_path)
+    return finetuned_model
 
 
 def find_best_finetuning_params(search_space: dict, model: TrainedModel, max_evals=10, evaluate=True) -> Sweep:
    
     trials = Trials()
-    best_params = fmin(finetune_model, space=search_space, algo=tpe.suggest, max_evals=max_evals, trials=trials)
+    best_params = fmin(finetune_model_from_args, space=search_space, algo=tpe.suggest, max_evals=max_evals, trials=trials)
     # add basin back to best params    
     # add basin back to best params
 
     # run best model to get that fresh validation data
     best_args = param_dict_from_model_output(best_params, search_space['basin'])
-    finetuning_data = finetune_model(best_args)
+    finetuning_data = finetune_model_from_args(best_args)
     finetuned_model = finetuning_data['model']
 
     # store model, finetuned model, and best_params
@@ -111,9 +112,11 @@ def finetune_on_n_basins(model: TrainedModel, search_space: dict, n=500, max_eva
     
     for _ in range(n):
         # pick basin
-        basin, _ = pick_a_basin(model=model)
+        basin = pick_a_basin(model=model)
         while basin in basins:
-            basin, _ = pick_a_basin(model=model)
+            basin = pick_a_basin(model=model)
+        update_files(model=model, basin=basin)
+
         basins.append(basin)
         search_space['basin'] = basin
 
--- experiment/static_attributes/nh_attribute_tuning.py
+++ experiment/static_attributes/nh_attribute_tuning.py
@@ -2,8 +2,64 @@
 
 from pathlib import Path
 
-from neuralhydrology.nh_run import finetune
+import torch
 
-if __name__ == '__main__':
+from experiment import train
+from neuralhydrology.utils.config import Config
+from neuralhydrology.datasetzoo import get_dataset
+from neuralhydrology.datautils.utils import load_scaler
+
+from experiment.finetuning.finetune import finetune_model, pick_a_basin, update_files
+
+from experiment.utils import TrainedModelID, get_epoch_string, TrainedModel
+
+def finetune_attributes_for_basin(basin: str, base_model: TrainedModel) -> TrainedModel:
+    # create yaml from base_model and basin
     config_file_path = Path(__file__).parent.parent / 'finetuning' / 'assets' / 'attribute_tuning.yml'
-    model = finetune(config_file_path)
+    update_files(model=base_model, basin=basin, yml_file_path=config_file_path)
+
+    # finetune the model
+    trained_model = finetune_model(config_file_path)
+    return trained_model
+
+def fetch_tuned_attributes(trained_model: TrainedModel) -> tuple[torch.Tensor, torch.Tensor]:
+    # get the statics corrector tensor
+    
+    loaded_model = torch.load(str(trained_model.run_dir / f'model_epoch{get_epoch_string(trained_model.cfg.epochs)}.pt'), weights_only=False)
+    statics_corrector = loaded_model['embedding_net.statics_corrector'].detach().cpu()
+
+    # get the original static attributes
+    scaler = load_scaler(trained_model.run_dir)
+    dataset = get_dataset(trained_model.cfg, is_train=False, period='train', scaler=scaler)
+    
+    assert basin in dataset._attributes.keys(), f'basin ({basin}) not in the dataset attributes ({dataset._attributes})'
+
+    original_attributes = dataset._attributes[basin]
+
+    assert statics_corrector.shape == original_attributes.shape, f'statics corrector and original attributes are not of the same shape, {statics_corrector.shape} and {original_attributes.shape}, respectively'
+
+    final_attributes = statics_corrector + original_attributes
+
+    return final_attributes, original_attributes
+
+
+
+    # 
+if __name__ == '__main__':
+    # pick a base model
+    base_model = TrainedModel((TrainedModelID.ATTRIBUTE_TUNING))
+
+    # pick a basin
+    basin = pick_a_basin(base_model)
+
+    # train model to finetune attributes
+    trained_model = finetune_attributes_for_basin(basin=basin, base_model=base_model)
+
+    # get the new and old attributes to compare
+    new_attributes, old_attributes = fetch_tuned_attributes(trained_model=trained_model)
+
+
+
+
+    
+    
